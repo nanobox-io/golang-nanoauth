1dcb57865df33f1c44135b48fd1196518d716c2e
Braxton Huggins <braxton@pagodabox.com>
2015-12-15 14:08:52 -0700

    Allow certificates to be loaded from files
    Restructure things so they are not hardcoded.
    Make an Auth struct to hold important pieces of data.
    Hopefully didnt break things that were already using it.

--- generate.go
+++ generate.go
@@ -1,57 +1,54 @@
 package nanoauth
 
 import (
-  "crypto/tls"
-  "crypto/rand"
-  "crypto/rsa"
-  "crypto/x509"
-  "crypto/x509/pkix"
-  "encoding/pem"
-  "math/big"
-  "time"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"time"
 )
 
-func Generate() (*tls.Certificate, error) {
-  host := "gonano.io"
+func Generate(host string) (*tls.Certificate, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
 
-  priv, err := rsa.GenerateKey(rand.Reader, 2048)
-  if err != nil {
-    return nil, err
-  }
+	notBefore := time.Now()
 
-  notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * 100 * time.Hour) // 100 years..
 
-  notAfter := notBefore.Add(365*24*100*time.Hour) // 100 years..
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
 
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Acme Co"},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
 
-  serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-  serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-  if err != nil {
-    return nil, err
-  }
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
 
-  template := x509.Certificate{
-    SerialNumber: serialNumber,
-    Subject: pkix.Name{
-      Organization: []string{"Acme Co"},
-    },
-    NotBefore: notBefore,
-    NotAfter:  notAfter,
+	template.DNSNames = append(template.DNSNames, host)
 
-    KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-    ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-    BasicConstraintsValid: true,
-  }
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, err
+	}
 
-  template.DNSNames = append(template.DNSNames, host)
-
-  derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-  if err != nil {
-    return nil, err
-  }
-
-  cert := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-  key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-  c, err := tls.X509KeyPair(cert, key)
-  return &c, err
+	cert := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	c, err := tls.X509KeyPair(cert, key)
+	return &c, err
 }
---
+++ load.go
@@ -0,0 +1,28 @@
+package nanoauth
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+)
+
+func Load(certFile, keyFile, password string) (*tls.Certificate, error) {
+	certPEMBlock, err := ioutil.ReadFile(certFile)
+	if err != nil {
+		return &tls.Certificate{}, err
+	}
+	keyPEMBlock, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		return &tls.Certificate{}, err
+	}
+	temp, _ := pem.Decode(keyPEMBlock)
+	if x509.IsEncryptedPEMBlock(temp) {
+		keyPEMBlock, err = x509.DecryptPEMBlock(temp, []byte(password))
+		if err != nil {
+			return &tls.Certificate{}, err
+		}
+	}
+	crt, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
+	return &crt, err
+}
